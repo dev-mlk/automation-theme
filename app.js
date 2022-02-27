135fd7c17f96020c7341175f48494d9e0a819b52
dev-mlk <k.maxlandry@gmail.com>
2022-02-27 23:27:23 +0100

    first

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require("express")
+const path = require("path")
 
 const app = express()
 const PORT = 3007
 
+// app.get("/", (req, res) => {
+//     res.render('index')
+// })
+
 app.get("/", (req, res) => {
-    res.send('hello world!')
+    res.sendFile("index.html", {
+        root: path.join(__dirname, "public")
+    })
+})
+// app.get("/service", (req, res) => {
+//     res.render('services')
+// })
+
+app.get("/services", (req, res) => {
+    res.sendFile("services.html", {
+        root: path.join(__dirname, "public")
+    })
 })
 
 app.listen(PORT, () => {
-    console.log(`Server listening on https://localhost:${PORT}`)
+    console.log(`Server listening on http://localhost:${PORT}`)
 })
